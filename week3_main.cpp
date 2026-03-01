0ee89201f9d692d5016722afb559ffc6648b66d4
bkacquah <bkacquah83@gmail.com>
2026-03-01 09:44:35 -0800

    Update week3_main.cpp

--- week3_main.cpp
+++ week3_main.cpp
@@ -1,7 +1,7 @@
 /*
  * ELECTRICAL LOAD MONITORING SYSTEM
- * Author: LOVE FOBI
- * Index: 01240239D
+ * Author: BLANKSON KINGSFORD ACQUAH
+ * Index: 01241148D
  * Course: EEE 227
  * Week 3: Billing Calculation Added
  * Date: February 15, 2026
